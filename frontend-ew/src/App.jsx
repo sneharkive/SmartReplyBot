c1b8b02f653c96006b3a75922cbd9716ac210593
Sneha <sneharoy90737@gmail.com>
2025-07-07 19:45:36 +0530

    made change

--- frontend-ew/src/App.jsx
+++ frontend-ew/src/App.jsx
@@ -20,7 +20,7 @@ function App() {
       });
       setGeneratedReply(typeof response.data === 'string' ? response.data : JSON.stringify(response.data));
     } catch (error) {
-      setError('Failed to generate eamil reply. Please try again');
+      setError('Failed to generate email reply. Please try again');
       console.error(error);
     } finally {
       setLoading(false);
